e5c5f94a1041bfd052c7bd6de60e1998da853081
Beker94 <bekerskyi@gmail.com>
2021-02-17 21:27:11 +0200

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import { FunctionalComponent } from "./components/FunctionalComponent";
+import { Pure } from "./components/PureComponent";
+import { ClassComponent } from "./components/ClassComponent";
 
 function App() {
+  const element = React.createElement(
+    "h1",
+    { className: "CreateElemen" },
+    "Hello World(CreateElemen)"
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {element}
+      <ClassComponent />
+      <Pure />
+      <FunctionalComponent />
     </div>
   );
 }
---
+++ src/components/PureComponent/PureComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Pure extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello World(PureComponent)</h1>
+      </div>
+    );
+  }
+}
+
+export default Pure;
